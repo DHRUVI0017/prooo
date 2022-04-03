0a10bd24ab04a3426e3c15c356a4e6bec1b10b57
DHRUVI0017 <101318265+DHRUVI0017@users.noreply.github.com>
2022-04-03 14:54:50 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,42 @@
+import pygame, sys,random
+
+pygame.init()
+pygame.mixer.init()
+
+clock=pygame.time.Clock()
+width=600
+height=350
+screen = pygame.display.set_mode((width,height))
+  
+#load the images in dict
+images={}
+images["bg"] = pygame.image.load("city3.jpg").convert_alpha()
+images["bin"] = pygame.image.load("bin.png").convert_alpha()
+  
+
+class Dustbin:
+    def __init__(self,x):
+        self.bin=pygame.Rect(x,210,10,10)
+        
+    def display(self):
+        screen.blit(images["bin"],self.bin)
+        
+bin1=Dustbin(25)
+bin2=Dustbin(337)
+bin3=Dustbin(517)
+while True:    
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+            sys.exit() 
+    
+    
+    
+    screen.blit(images["bg"],[0,0])
+
+    bin1.display()
+    bin2.display()
+    bin3.display()
+    pygame.display.update()
+    clock.tick(30) 
